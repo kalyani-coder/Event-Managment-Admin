c3cd4b161a5faed6a3749c179c779c3c1ec4929b
Dhairyasheel4Ingale <123227878+Dhairyasheel4Ingale@users.noreply.github.com>
2023-09-06 10:31:56 +0530

    Added backend functionality for following tables:
    /api/enquiry
    /api/quotation
    /api/advpayment
    /api/event
    /api/order
    /api/eventexpense
    /api/manager
    /api/executive
    /api/vendor
    /api/managerdetails
    /api/executivedetails
    /api/staffsalary
    /api/staffsalarydetails
    /api/vendorpayment
    /api/vendorpaymenthistory

---
+++ backend_MongoDB/models/newModels.js
@@ -0,0 +1,209 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EnquirySchema = new Schema({
+  enquiry_id: Number,
+  event_name: String,
+  event_date: String,
+  guest_quantity: Number,
+  event_venue: String,
+  event_requirement: String,
+});
+
+const QuotationSchema = new Schema({
+  quotation_id: Number,
+  enquiry_id: { type: Number, ref: "Enquiry" },
+  customer_name: String,
+  email: String,
+  contact: Number,
+  address: String,
+  event_type: String,
+  amount: Number,
+  tax: Number,
+  charges: Number,
+  total_amount: Number,
+});
+
+const AdvPaymentSchema = new Schema({
+  id: Number,
+  enquiry_id: { type: Number, ref: "Enquiry" },
+  event_name: String,
+  client_name: String,
+  amount: Number,
+  adv_payment: Number,
+  payment_date: String,
+  rem_payment: Number,
+  details: String,
+});
+
+const EventSchema = new Schema({
+  event_id: Number,
+  quotation_id: { type: Number, ref: "Quotation" },
+  fname: String,
+  company_name: String,
+  email: String,
+  contact: Number,
+  event_type: String,
+  event_date: String,
+  venue: String,
+  subvenue: String,
+  guest_number: Number,
+  budget: Number,
+});
+
+const OrderSchema = new Schema({
+  order_id: Number,
+  event_id: { type: Number, ref: "Event" },
+  customer_name: String,
+  contact: Number,
+  email: String,
+  date: String,
+  venue: String,
+  adv_payment: Number,
+  rem_payment: Number,
+  total_amt: String,
+  status: String,
+});
+
+const EventExpenseSchema = new Schema({
+  expense_id: Number,
+  event_id: { type: Number, ref: "Event" },
+  new_purchase: String,
+  to_vendor: String,
+  event_name: String,
+  amount: Number,
+  date: String,
+  payment_details: String,
+});
+
+const ManagerSchema = new Schema({
+  manager_id: Number,
+  fname: String,
+  lname: String,
+  email: String,
+  contact: Number,
+  profile_image: String,
+  pan_no: String,
+  state: String,
+  city: String,
+  address: String,
+  blood_group: String,
+});
+
+const ExecutiveSchema = new Schema({
+  executive_id: Number,
+  fname: String,
+  lname: String,
+  email: String,
+  contact: Number,
+  profile_image: String,
+  pan_no: String,
+  city: String,
+  state: String,
+  address: String,
+  blood_group: String,
+});
+
+const VendorSchema = new Schema({
+  vendor_id: Number,
+  company_name: String,
+  contact_person_name: String,
+  gmail: String,
+  contact: Number,
+  profile_image: String,
+  gst_no: Number,
+  pan_no: String,
+  address: String,
+});
+
+const ManagerDetailsSchema = new Schema({
+  manager_id: { type: Number, ref: "Manager" },
+  fname: String,
+  lname: String,
+  email: String,
+  contact: Number,
+  profile_image: String,
+  address: String,
+  city: String,
+  state: String,
+});
+
+const ExecutiveDetailsSchema = new Schema({
+  executive_id: { type: Number, ref: "Executive" },
+  fname: String,
+  lname: String,
+  email: String,
+  contact: Number,
+  profile_image: String,
+  address: String,
+  city: String,
+  state: String,
+});
+
+const StaffSalarySchema = new Schema({
+  staff_id: Number,
+  fname: String,
+  lname: String,
+  salary: Number,
+  date: String,
+  month: String,
+  adv_payment: Number,
+  rem_payment: Number,
+  incentive: Number,
+  deduct_amount: Number,
+});
+
+const StaffSalaryDetailsSchema = new Schema({
+  staff_id: { type: Number, ref: "StaffSalary" },
+  name: String,
+  salary: Number,
+  date: String,
+  month: String,
+});
+
+const VendorPaymentSchema = new Schema({
+  vendor_id: { type: Number, ref: "Vendor" },
+  id: Number,
+  event_id: { type: Number, ref: "Event" },
+  fname: String,
+  lname: String,
+  event_name: String,
+  paid_amt: Number,
+  rem_amt: Number,
+  date: String,
+  description: String,
+});
+
+const VendorPaymentHistorySchema = new Schema({
+  vendor_id: { type: Number, ref: "Vendor" },
+  name: String,
+  amt_paid: Number,
+  rem_amt: Number,
+  event_name: String,
+  date: String,
+  total_amount: Number,
+});
+
+module.exports = {
+  Enquiry: mongoose.model("Enquiry", EnquirySchema),
+  Quotation: mongoose.model("Quotation", QuotationSchema),
+  AdvPayment: mongoose.model("AdvPayment", AdvPaymentSchema),
+  Event: mongoose.model("Event", EventSchema),
+  Order: mongoose.model("Order", OrderSchema),
+  EventExpense: mongoose.model("EventExpense", EventExpenseSchema),
+  Manager: mongoose.model("Manager", ManagerSchema),
+  Executive: mongoose.model("Executive", ExecutiveSchema),
+  Vendor: mongoose.model("Vendor", VendorSchema),
+  ManagerDetails: mongoose.model("ManagerDetails", ManagerDetailsSchema),
+  ExecutiveDetails: mongoose.model("ExecutiveDetails", ExecutiveDetailsSchema),
+  StaffSalary: mongoose.model("StaffSalary", StaffSalarySchema),
+  StaffSalaryDetails: mongoose.model(
+    "StaffSalaryDetails",
+    StaffSalaryDetailsSchema
+  ),
+  VendorPayment: mongoose.model("VendorPayment", VendorPaymentSchema),
+  VendorPaymentHistory: mongoose.model(
+    "VendorPaymentHistory",
+    VendorPaymentHistorySchema
+  ),
+};
--- backend_MongoDB/utils/utils.js
+++ backend_MongoDB/utils/utils.js
@@ -1,8 +1,30 @@
+const {
+  Enquiry,
+  Quotation,
+  AdvPayment,
+  Event,
+  Order,
+  EventExpense,
+  Manager,
+  Executive,
+  Vendor,
+  ManagerDetails,
+  ExecutiveDetails,
+  StaffSalary,
+  StaffSalaryDetails,
+  VendorPayment,
+  VendorPaymentHistory,
+} = require("../models/newModels");
+
 const Accountants = require("../models/accountant");
 const Enquiries = require("../models/enquiry");
+
 const Executives = require("../models/executive");
+
 const Vendors = require("../models/vendor");
+
 const Managers = require("../models/manager");
+const Events = require("../models/event");
 
 const FindTable = ({ table }) => {
   if (table.toLowerCase() === "accountants") {
@@ -15,6 +37,38 @@ const FindTable = ({ table }) => {
     return Vendors;
   } else if (table.toLowerCase() === "managers") {
     return Managers;
+  } else if (table.toLowerCase() === "events") {
+    return Events; //<---------------------------------------------------------- new models begin after this else if block
+  } else if (table.toLowerCase() === "enquiry") {
+    return Enquiry;
+  } else if (table.toLowerCase() === "quotation") {
+    return Quotation;
+  } else if (table.toLowerCase() === "advpayment") {
+    return AdvPayment;
+  } else if (table.toLowerCase() === "event") {
+    return Event;
+  } else if (table.toLowerCase() === "order") {
+    return Order;
+  } else if (table.toLowerCase() === "eventexpense") {
+    return EventExpense;
+  } else if (table.toLowerCase() === "manager") {
+    return Manager;
+  } else if (table.toLowerCase() === "executive") {
+    return Executive;
+  } else if (table.toLowerCase() === "vendor") {
+    return Vendor;
+  } else if (table.toLowerCase() === "managerdetails") {
+    return ManagerDetails;
+  } else if (table.toLowerCase() === "executivedetails") {
+    return ExecutiveDetails;
+  } else if (table.toLowerCase() === "staffsalary") {
+    return StaffSalary;
+  } else if (table.toLowerCase() === "staffsalarydetails") {
+    return StaffSalaryDetails;
+  } else if (table.toLowerCase() === "vendorpayment") {
+    return VendorPayment;
+  } else if (table.toLowerCase() === "vendorpaymenthistory") {
+    return VendorPaymentHistory;
   } else {
     return null;
   }
@@ -165,6 +219,372 @@ const FilterBodyByTable = ({ req, table }) => {
       bankName,
       branchName,
       profilePicture,
+    };
+  } else if (table == "events") {
+    const {
+      eventName,
+      fullName,
+      companyName,
+      email,
+      contactNumber,
+      eventType,
+      selectedVenue,
+      subVenue,
+      numOfGuests,
+      budget,
+      currentDate,
+      currentTime,
+    } = req.body;
+    return {
+      eventName,
+      fullName,
+      companyName,
+      email,
+      contactNumber,
+      eventType,
+      selectedVenue,
+      subVenue,
+      numOfGuests,
+      budget,
+      currentDate,
+      currentTime,
+    };
+  } else if (table == "enquiry") {
+    const {
+      enquiry_id,
+      event_name,
+      event_date,
+      guest_quantity,
+      event_venue,
+      event_requirement,
+    } = req.body;
+    return {
+      enquiry_id,
+      event_name,
+      event_date,
+      guest_quantity,
+      event_venue,
+      event_requirement,
+    };
+  } else if (table == "quotation") {
+    const {
+      quotation_id,
+      enquiry_id,
+      customer_name,
+      email,
+      contact,
+      address,
+      event_type,
+      amount,
+      tax,
+      charges,
+      total_amount,
+    } = req.body;
+    return {
+      quotation_id,
+      enquiry_id,
+      customer_name,
+      email,
+      contact,
+      address,
+      event_type,
+      amount,
+      tax,
+      charges,
+      total_amount,
+    };
+  } else if (table == "advpayment") {
+    const {
+      id,
+      enquiry_id,
+      event_name,
+      client_name,
+      amount,
+      adv_payment,
+      payment_date,
+      rem_payment,
+      details,
+    } = req.body;
+    return {
+      id,
+      enquiry_id,
+      event_name,
+      client_name,
+      amount,
+      adv_payment,
+      payment_date,
+      rem_payment,
+      details,
+    };
+  } else if (table == "event") {
+    const {
+      event_id,
+      quotation_id,
+      fname,
+      company_name,
+      email,
+      contact,
+      event_type,
+      event_date,
+      venue,
+      subvenue,
+      guest_number,
+      budget,
+    } = req.body;
+    return {
+      event_id,
+      quotation_id,
+      fname,
+      company_name,
+      email,
+      contact,
+      event_type,
+      event_date,
+      venue,
+      subvenue,
+      guest_number,
+      budget,
+    };
+  } else if (table == "order") {
+    const {
+      order_id,
+      event_id,
+      customer_name,
+      contact,
+      email,
+      date,
+      venue,
+      adv_payment,
+      rem_payment,
+      total_amt,
+      status,
+    } = req.body;
+    return {
+      order_id,
+      event_id,
+      customer_name,
+      contact,
+      email,
+      date,
+      venue,
+      adv_payment,
+      rem_payment,
+      total_amt,
+      status,
+    };
+  } else if (table == "eventexpense") {
+    const {
+      expense_id,
+      event_id,
+      new_purchase,
+      to_vendor,
+      event_name,
+      amount,
+      date,
+      payment_details,
+    } = req.body;
+    return {
+      expense_id,
+      event_id,
+      new_purchase,
+      to_vendor,
+      event_name,
+      amount,
+      date,
+      payment_details,
+    };
+  } else if (table == "manager") {
+    const {
+      manager_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      pan_no,
+      state,
+      city,
+      address,
+      blood_group,
+    } = req.body;
+    return {
+      manager_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      pan_no,
+      state,
+      city,
+      address,
+      blood_group,
+    };
+  } else if (table == "executive") {
+    const {
+      executive_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      pan_no,
+      city,
+      state,
+      address,
+      blood_group,
+    } = req.body;
+    return {
+      executive_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      pan_no,
+      city,
+      state,
+      address,
+      blood_group,
+    };
+  } else if (table == "vendor") {
+    const {
+      vendor_id,
+      company_name,
+      contact_person_name,
+      gmail,
+      contact,
+      profile_image,
+      gst_no,
+      pan_no,
+      address,
+    } = req.body;
+    return {
+      vendor_id,
+      company_name,
+      contact_person_name,
+      gmail,
+      contact,
+      profile_image,
+      gst_no,
+      pan_no,
+      address,
+    };
+  } else if (table == "managerdetails") {
+    const {
+      manager_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      address,
+      state,
+    } = req.body;
+    return {
+      manager_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      address,
+      state,
+    };
+  } else if (table == "executivedetails") {
+    const {
+      executive_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      address,
+      city,
+      state,
+    } = req.body;
+    return {
+      executive_id,
+      fname,
+      lname,
+      email,
+      contact,
+      profile_image,
+      address,
+      city,
+      state,
+    };
+  } else if (table == "staffsalary") {
+    const {
+      staff_id,
+      fname,
+      lname,
+      salary,
+      date,
+      month,
+      adv_payment,
+      rem_payment,
+      incentive,
+      deduct_amount,
+    } = req.body;
+    return {
+      staff_id,
+      fname,
+      lname,
+      salary,
+      date,
+      month,
+      adv_payment,
+      rem_payment,
+      incentive,
+      deduct_amount,
+    };
+  } else if (table == "staffsalarydetails") {
+    const { staff_id, name, salary, date, month } = req.body;
+    return { staff_id, name, salary, date, month };
+  } else if (table == "vendorpayment") {
+    const {
+      vendor_id,
+      id,
+      event_id,
+      fname,
+      lname,
+      event_name,
+      paid_amt,
+      rem_amt,
+      date,
+      description,
+    } = req.body;
+    return {
+      vendor_id,
+      id,
+      event_id,
+      fname,
+      lname,
+      event_name,
+      paid_amt,
+      rem_amt,
+      date,
+      description,
+    };
+  } else if (table == "vendorpaymenthistory") {
+    const {
+      vendor_id,
+      name,
+      amt_paid,
+      rem_amt,
+      event_name,
+      date,
+      total_amount,
+    } = req.body;
+    return {
+      vendor_id,
+      name,
+      amt_paid,
+      rem_amt,
+      event_name,
+      date,
+      total_amount,
     };
   } else {
     return null;
