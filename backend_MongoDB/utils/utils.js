515c046623b00468182e0099903c5401b5624b83
Dhairyasheel4Ingale <123227878+Dhairyasheel4Ingale@users.noreply.github.com>
2023-09-13 13:37:02 +0530

    New res.body syntax:
    "enquiry"
        const {
          enquiry_id,
          event_name,
          event_date,
          guest_quantity,
          event_venue,
          event_requirement,
        } = req.body;
    
    "quotation"
        const {
          quotation_id,
          enquiry_id,
          customer_name,
          email,
          contact,
          address,
          event_type,
          amount,
          tax,
          charges,
          total_amount,
        } = req.body;
    
    "advpayment"
        const {
          id,
          enquiry_id,
          event_name,
          client_name,
          amount,
          adv_payment,
          payment_date,
          rem_payment,
          details,
        } = req.body;
    
    "event"
        const {
          event_id,
          quotation_id,
          fname,
          company_name,
          email,
          contact,
          event_type,
          event_date,
          venue,
          subvenue,
          guest_number,
          budget,
        } = req.body;
    
    "order"
        const {
          order_id,
          event_id,
          customer_name,
          contact,
          email,
          date,
          venue,
          adv_payment,
          rem_payment,
          total_amt,
          status,
        } = req.body;
    
    "eventexpense"
        const {
          expense_id,
          event_id,
          new_purchase,
          to_vendor,
          event_name,
          amount,
          date,
          payment_details,
        } = req.body;
    
    "manager"
        const {
          manager_id,
          fname,
          lname,
          email,
          contact,
          profile_image,
          pan_no,
          state,
          city,
          address,
          blood_group,
        } = req.body;
    
    "executive"
        const {
          executive_id,
          fname,
          lname,
          email,
          contact,
          profile_image,
          pan_no,
          city,
          state,
          address,
          blood_group,
        } = req.body;
    
    "vendor"
        const {
          vendor_id,
          company_name,
          contact_person_name,
          gmail,
          contact,
          profile_image,
          gst_no,
          pan_no,
          address,
        } = req.body;
    
    "managerdetails"
        const {
          manager_id,
          fname,
          lname,
          email,
          contact,
          profile_image,
          address,
          state,
        } = req.body;
    
    "executivedetails"
        const {
          executive_id,
          fname,
          lname,
          email,
          contact,
          profile_image,
          address,
          city,
          state,
        } = req.body;
    
    "staffsalary"
        const {
          staff_id,
          fname,
          lname,
          salary,
          date,
          month,
          adv_payment,
          rem_payment,
          incentive,
          deduct_amount,
        } = req.body;
    
        "staffsalarydetails"
        const { staff_id, name, salary, date, month } = req.body
    
    "vendorpayment"
        const {
          vendor_id,
          id,
          event_id,
          fname,
          lname,
          event_name,
          paid_amt,
          rem_amt,
          date,
          description,
        } = req.body;
    
    "vendorpaymenthistory"
        const {
          vendor_id,
          name,
          amt_paid,
          rem_amt,
          event_name,
          date,
          total_amount,
        } = req.body;

--- backend_MongoDB/utils/utils.js
+++ backend_MongoDB/utils/utils.js
@@ -16,28 +16,18 @@ const {
   VendorPaymentHistory,
 } = require("../models/newModels");
 
-const Accountants = require("../models/accountant");
-const Enquiries = require("../models/enquiry");
+// const Accountants = require("../models/accountant");
+// const Enquiries = require("../models/enquiry");
 
-const Executives = require("../models/executive");
+// const Executives = require("../models/executive");
 
-const Vendors = require("../models/vendor");
+// const Vendors = require("../models/vendor");
 
-const Managers = require("../models/manager");
+// const Managers = require("../models/manager");
 // const Events = require("../models/event");
 
 const FindTable = ({ table }) => {
-  if (table.toLowerCase() === "accountants") {
-    return Accountants;
-  } else if (table.toLowerCase() === "enquiries") {
-    return Enquiries;
-  } else if (table.toLowerCase() === "executives") {
-    return Executives;
-  } else if (table.toLowerCase() === "vendors") {
-    return Vendors;
-  } else if (table.toLowerCase() === "managers") {
-    return Managers; //<---------------------------------------------------------- new models begin after this else if block
-  } else if (table.toLowerCase() === "enquiry") {
+  if (table.toLowerCase() === "enquiry") {
     return Enquiry;
   } else if (table.toLowerCase() === "quotation") {
     return Quotation;
@@ -73,181 +63,7 @@ const FindTable = ({ table }) => {
 };
 
 const FilterBodyByTable = ({ req, table }) => {
-  if (table === "accountants") {
-    const {
-      accountHolderName,
-      accountNumber,
-      accountantAddress,
-      accountantCity,
-      accountantEmail,
-      accountantPhone,
-      accountantState,
-      bankName,
-      branchName,
-      firstName,
-      ifscCode,
-      lastName,
-      profilePicture,
-    } = req.body;
-    return {
-      accountHolderName,
-      accountNumber,
-      accountantAddress,
-      accountantCity,
-      accountantEmail,
-      accountantPhone,
-      accountantState,
-      bankName,
-      branchName,
-      firstName,
-      ifscCode,
-      lastName,
-      profilePicture,
-    };
-  } else if (table === "enquiries") {
-    const {
-      title,
-      eventName,
-      customerName,
-      customerEmail,
-      contactNumber,
-      customerAddress,
-      eventDate,
-      numberOfGuests,
-      eventVenue,
-      eventRequirement,
-    } = req.body;
-    return {
-      title,
-      eventName,
-      customerName,
-      customerEmail,
-      contactNumber,
-      customerAddress,
-      eventDate,
-      numberOfGuests,
-      eventVenue,
-      eventRequirement,
-    };
-  } else if (table === "executives") {
-    const {
-      accountHolderName,
-      accountNumber,
-      bankName,
-      branchName,
-      executiveAddress,
-      executiveCity,
-      executiveEmail,
-      executivePhone,
-      executiveState,
-      firstName,
-      ifscCode,
-      lastName,
-      profilePicture,
-    } = req.body;
-    return {
-      accountHolderName,
-      accountNumber,
-      bankName,
-      branchName,
-      executiveAddress,
-      executiveCity,
-      executiveEmail,
-      executivePhone,
-      executiveState,
-      firstName,
-      ifscCode,
-      lastName,
-      profilePicture,
-    };
-  } else if (table === "vendors") {
-    const {
-      vendorCategory,
-      vendorCompanyName,
-      vendorContactPerson,
-      vendorEmail,
-      vendorPhone,
-      vendorAddress,
-      vendorCity,
-      vendorState,
-      gstNumber,
-      panNumber,
-      profilePicture,
-    } = req.body;
-    return {
-      vendorCategory,
-      vendorCompanyName,
-      vendorContactPerson,
-      vendorEmail,
-      vendorPhone,
-      vendorAddress,
-      vendorCity,
-      vendorState,
-      gstNumber,
-      panNumber,
-      profilePicture,
-    };
-  } else if (table === "managers") {
-    const {
-      firstName,
-      lastName,
-      managerEmail,
-      managerPhone,
-      managerAddress,
-      managerCity,
-      managerState,
-      accountHolderName,
-      accountNumber,
-      ifscCode,
-      bankName,
-      branchName,
-      profilePicture,
-    } = req.body;
-    return {
-      firstName,
-      lastName,
-      managerEmail,
-      managerPhone,
-      managerAddress,
-      managerCity,
-      managerState,
-      accountHolderName,
-      accountNumber,
-      ifscCode,
-      bankName,
-      branchName,
-      profilePicture,
-    };
-  } else if (table == "events") {
-    const {
-      eventName,
-      fullName,
-      companyName,
-      email,
-      contactNumber,
-      eventType,
-      selectedVenue,
-      subVenue,
-      numOfGuests,
-      budget,
-      currentDate,
-      currentTime,
-    } = req.body;
-    return {
-      eventName,
-      fullName,
-      companyName,
-      email,
-      contactNumber,
-      eventType,
-      selectedVenue,
-      subVenue,
-      numOfGuests,
-      budget,
-      currentDate,
-      currentTime,
-    };
-  } else if (table == "enquiry") {
+  if (table == "enquiry") {
     const {
       enquiry_id,
       event_name,
