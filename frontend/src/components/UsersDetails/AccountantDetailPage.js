12e73f5eae1532657262d60bea48f688ae6206b2
prasadrahane06 <prasadrahane06@gmail.com>
2024-04-26 14:46:43 +0530

    update AccountantDetailPage.js and ManagerDetailPage.js

--- frontend/src/components/UsersDetails/AccountantDetailPage.js
+++ frontend/src/components/UsersDetails/AccountantDetailPage.js
@@ -44,46 +44,57 @@ const AccountantDetailPage = () => {
   return (
     <>
       <Header />
-    <div className="container mt-5">
-      <h2>
-        {accountant.fname} {accountant.lname} Details
-      </h2>
-      <Card style={{ width: "100%" }}>
-        <Card.Body>
-          <Card.Title>{`${accountant.fname} ${accountant.lname}`}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Contact Number: {accountant.contact}
-          </Card.Subtitle>
-          <Card.Text>Address: {accountant.address}</Card.Text>
-          <Card.Text>Email: {accountant.email}</Card.Text>
-          <Card.Text>City: {accountant.city}</Card.Text>
-          <Card.Text>State: {accountant.state}</Card.Text>
-          <div className="my-3">
-            <hr />
-            <h6 className="mb-3">Bank details:</h6>
-            <Card.Text>
-              Account Holder Name: {accountant.holder_name}
-            </Card.Text>
-            <Card.Text>Account Number: {accountant.account_number}</Card.Text>
-            <Card.Text>IFSC Code: {accountant.IFSC_code}</Card.Text>
-            <Card.Text>Bank Name: {accountant.bank_name}</Card.Text>
-            <Card.Text>Branch Name: {accountant.branch_name}</Card.Text>
-            <hr />
-          </div>
-          <div className="my-3">
-            <Button variant="primary" onClick={handleEdit}>
-              Edit
-            </Button>{" "}
-            <Button variant="danger" className="mx-1" onClick={handleDelete}>
-              Delete
-            </Button>
-            <Button variant="info" className="mx-1" onClick={handleSalary}>
-              Add Salary
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
+      <div
+        className="w-full h-screen
+        flex items-center justify-center main-container-for-Addaccount overflow-y-auto "
+      >
+        <div className="md:h-[80vh] h-[80vh] md:mt-0 md:w-[50%]  min-w-3 px-3 ml-3  ">
+          <h2 className="text-[35px]">
+            {accountant.fname} {accountant.lname} Details
+          </h2>
+          <Card style={{ width: "100%" }}>
+            <Card.Body>
+              <Card.Title>{`${accountant.fname} ${accountant.lname}`}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Contact Number: {accountant.contact}
+              </Card.Subtitle>
+              <Card.Text>Address: {accountant.address}</Card.Text>
+              <Card.Text>Email: {accountant.email}</Card.Text>
+              <Card.Text>City: {accountant.city}</Card.Text>
+              <Card.Text>State: {accountant.state}</Card.Text>
+              <div className="my-3">
+                <hr />
+                <h6 className="mb-3">Bank details:</h6>
+                <Card.Text>
+                  Account Holder Name: {accountant.holder_name}
+                </Card.Text>
+                <Card.Text>
+                  Account Number: {accountant.account_number}
+                </Card.Text>
+                <Card.Text>IFSC Code: {accountant.IFSC_code}</Card.Text>
+                <Card.Text>Bank Name: {accountant.bank_name}</Card.Text>
+                <Card.Text>Branch Name: {accountant.branch_name}</Card.Text>
+                <hr />
+              </div>
+              <div className="my-3 grid gap-1 md:flex">
+                <Button variant="primary" onClick={handleEdit}>
+                  Edit
+                </Button>{" "}
+                <Button
+                  variant="danger"
+                  className="mx-1"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </Button>
+                <Button variant="info" className="mx-1" onClick={handleSalary}>
+                  Add Salary
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/components/UsersDetails/ManagerDetailPage.js
+++ frontend/src/components/UsersDetails/ManagerDetailPage.js
@@ -51,46 +51,55 @@ const ManagerDetailPage = () => {
   return (
     <>
       <Header />
-    <div className="container mt-5">
-      <h2>
-        {manager.fname} {manager.lname} Details
-      </h2>
-      <Card style={{ width: "100%" }}>
-        <Card.Body>
-          <Card.Title>{`${manager.fname} ${manager.lname}`}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Contact Number: {manager.contact}
-          </Card.Subtitle>
-          <Card.Text>Address: {manager.address}</Card.Text>
-          <Card.Text>Email: {manager.email}</Card.Text>
-          <Card.Text>City: {manager.city}</Card.Text>
-          <Card.Text>State: {manager?.state}</Card.Text>
-          <div className="my-3">
-            <hr />
-            <h6 className="mb-3">Bank details:</h6>
-            <Card.Text>
-              Account Holder Name: {manager.holder_name}
-            </Card.Text>
-            <Card.Text>Account Number: {manager.account_number}</Card.Text>
-            <Card.Text>IFSC Code: {manager.IFSC_code}</Card.Text>
-            <Card.Text>Bank Name: {manager.bank_name}</Card.Text>
-            <Card.Text>Branch Name: {manager.branch_name}</Card.Text>
-            <hr />
-          </div>
-          <div className="my-3">
-            <Button variant="info" onClick={handleEdit}>
-              Edit
-            </Button>{" "}
-            <Button variant="danger" className="mx-1" onClick={handleDelete}>
-              Delete
-            </Button>
-            <Button variant="info" className="mx-1" onClick={handleSalary}>
-              Add Salary
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
+      <div
+        className="w-full h-screen
+        flex items-center justify-center main-container-for-Addaccount overflow-y-auto "
+      >
+        <div className="md:h-[80vh] h-[80vh] md:mt-0 md:w-[50%]  min-w-3 px-3 ml-3  ">
+          <h2 className="text-[35px]">
+            {manager.fname} {manager.lname} Details
+          </h2>
+          <Card style={{ width: "100%" }}>
+            <Card.Body>
+              <Card.Title>{`${manager.fname} ${manager.lname}`}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Contact Number: {manager.contact}
+              </Card.Subtitle>
+              <Card.Text>Address: {manager.address}</Card.Text>
+              <Card.Text>Email: {manager.email}</Card.Text>
+              <Card.Text>City: {manager.city}</Card.Text>
+              <Card.Text>State: {manager?.state}</Card.Text>
+              <div className="my-3">
+                <hr />
+                <h6 className="mb-3">Bank details:</h6>
+                <Card.Text>
+                  Account Holder Name: {manager.holder_name}
+                </Card.Text>
+                <Card.Text>Account Number: {manager.account_number}</Card.Text>
+                <Card.Text>IFSC Code: {manager.IFSC_code}</Card.Text>
+                <Card.Text>Bank Name: {manager.bank_name}</Card.Text>
+                <Card.Text>Branch Name: {manager.branch_name}</Card.Text>
+                <hr />
+              </div>
+              <div className="my-3 grid gap-1 md:flex">
+                <Button variant="info" onClick={handleEdit}>
+                  Edit
+                </Button>{" "}
+                <Button
+                  variant="danger"
+                  className="mx-1"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </Button>
+                <Button variant="info" className="mx-1" onClick={handleSalary}>
+                  Add Salary
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
