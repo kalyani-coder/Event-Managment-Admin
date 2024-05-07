2744a5414f5f922db2bbe92d33740b13153d8975
PoojaPatil0103 <pooja.patil10393@gmail.com>
2024-05-07 17:01:39 +0530

    update AdvancePaymnetCus.js and MakeQuotation.js

--- frontend/src/components/AddNewEvent/AdvancePaymnetCus.js
+++ frontend/src/components/AddNewEvent/AdvancePaymnetCus.js
@@ -109,30 +109,29 @@ function AdvancePaymnetCus() {
       payment_date: formattedDate,
       payment_time: currentTime,
       payment_method: paymentMethod,
-      customer_name: selectedCustomerDetails.customer_name,
-      contact: selectedCustomerDetails.contact,
-      email: selectedCustomerDetails.email,
-      date: selectedCustomerDetails.date,
-      venue: selectedCustomerDetails.venue,
-      event_name: selectedCustomerDetails.event_name,
-      event_Type: selectedCustomerDetails.event_Type,
-      guest_Number: selectedCustomerDetails.guest_Number,
-      assign_manager_name: "",
-      assign_manager_Id: "",
+      ...(paymentMethod === "cheque" && {
+        cheque_number: chequeNumber,
+        whome_to_submit: submittedTo,
+        utrno_rtgs_id: utrNumber,
+      }),
+      ...(paymentMethod === "cash" && {
+        cash_whome_to_submit: cash,
+      }),
+      ...(paymentMethod === "netbanking" && {
+        transaction_id: transaction,
+      }),
     };
 
     axios
       .post("http://localhost:5000/api/advpayment", data)
       .then((response) => {
-        // Display alert box after successfully saving data
-        alert("Customer payment successfully saved.");
-        setShowModal(true);
-        fetchManagers();
+        console.log("Data saved successfully:", response.data);
+        alert("Advance payment successfull");
+        navigate("/assignmanager");
       })
       .catch((error) => {
         console.error("Error saving data:", error);
-        setAlertMessage("Failed to save customer payment.");
-        setAlertVariant("danger");
+        // Optionally, handle error or display error message to the user
       });
   };
 
